66026e9cfac9705d96da0180eeaa8a5ebac67f87
kushal81 <83442979+kushal81@users.noreply.github.com>
2021-10-10 16:29:56 +0530

    Update help.js

--- commands/Information/help.js
+++ commands/Information/help.js
@@ -12,8 +12,8 @@ module.exports = {
       try{
         let string = `:white_check_mark: Type \`${prefix}commands\` for a list of Commands\n\n:question: New to **Milrato x Rythm**? Check out our [Rythm's FAQ](https://rythm.fm/docs/faq/)\n\n:page_facing_up: Still need help? [Click here](https://discord.gg/wvCp7q88G3) to join our [Discord server](https://discord.gg/wvCp7q88G3)`
         let embed = new MessageEmbed()
-        .setTitle("Milrato x Rythm Help")
-        .setURL("https://milrato.eu")
+        .setTitle("Kushal x Rythm Help")
+        .setURL("https://YouTube.com/KushalTheAndroidGuru")
         .setDescription(string)
         if(message.guild.me.permissionsIn(message.channel).has("EMBED_LINKS")){
           message.reply(embed)
